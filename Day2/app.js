76d80e5cb7c55894013ab88882560bc8e9b2ca91
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-24 14:25:20 +0530

    Day2

--- Day2/app.js
+++ Day2/app.js
@@ -36,19 +36,77 @@
 //true,false,null,undefined(render honge but kuch display nhi hoga)
 
 // const age = 18;
-const isLoggedIn = false;
+// const isLoggedIn = false;
 // const element = <h1>Hello Coder Army {10} {"Devu"} {[20,40,50]}</h1>
 // const element = <h1>Hello Coder {age>18?"Adult":"Kid"}</h1>
-const element = <h1>Hello Coder {isLoggedIn?<h2>Yes,Logged in</h2>:<h2>Kindlly SignIn</h2>}</h1>
+// const element = <h1>Hello Coder {isLoggedIn?<h2>Yes,Logged in</h2>:<h2>Kindlly SignIn</h2>}</h1>
 
+
+// COMPONENTS
+
+// //[<li>HTML</li>,<li>CSS</li>,<li>Javascript</li>]
+// const element = (
+// <ul>
+//     {[<li>HTML</li>,<li>CSS</li>,<li>Javascript</li>]}
+// </ul>)
+
+// const element = (
+// <ul>
+//     {courses.map(course=><li>{course}</li>)}
+// </ul>)
+
+
+// PROPS
+
+// const ab = {backgroundColor:"orange", color:"white"}
+// const element = <h1 id="title" className="first" style={ab}>Hello Coder Army</h1>
+
+// function App(props){
+
+//     return (
+//         <h1>Hello Nallo{props.name} {props.age}</h1>
+//     )
+// }
+
+// const element = <App name="Raghu" age={30}></App>
+
+
+function Header({name}){
+    return(
+        <h1>{name} Welcome to Indian Election Commision</h1>
+    )
+}
+
+function Main(){
+    return(
+        <h2>This is information about us</h2>
+    )
+}
+
+function Footer(){
+    return(
+        <h3>Thanks for visiting our website</h3>
+    )
+}
+
+function App(){
+
+    return(
+        <>
+          <Header name="Rohit"></Header>
+          <Main></Main>
+          <Footer></Footer>
+        </>
+    )
+}
 
 const root =ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(element);
+// root.render(element);
 // root.render(element2);
 // root.render(App()); //One more way to call 
-// root.render(<App/>);
-root.render(a);
+ root.render(<App/>);
+// root.render(a);
 
 
 

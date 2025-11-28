b888ff5cf665d2b0e85b41995f2726986ebf6e67
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-29 01:06:41 +0530

    Day4

---
+++ Day4.1/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+  function App(){
+    
+    let count = 0;
+
+    return (
+    <>
+      <p>Counter: {count}</p>
+      <button>Increment</button>
+     </>
+    )
+ }
+
+
+
+ export default App;

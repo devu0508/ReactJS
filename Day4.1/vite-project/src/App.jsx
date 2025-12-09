6109005da979d5b38fd7783a969e0007b08e6497
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-12-10 00:07:28 +0530

    Day4 complete

--- Day4.1/vite-project/src/App.jsx
+++ Day4.1/vite-project/src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 
   function App(){
+
+  let [count, setCount] = useState(0);
     
-    let count = 0;
+    //  let count = 0;
+
+    // function increaseNumber(){
+    //   count++;
+    //   // console.log(count);
+    //   const para = document.querySelector('p');
+    //   para.textContent = `Counter: ${count}`
+    // }
+
+    function increaseNumber(){
+      count++;
+      setCount(count);
+    }
 
     return (
     <>
       <p>Counter: {count}</p>
-      <button>Increment</button>
+      <button onClick={increaseNumber}>Increment</button>
      </>
     )
  }
